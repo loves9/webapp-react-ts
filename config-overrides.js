c2888f7b12aa7e20e0538978584b74ccaf90c718
loves9 <ifengfree@gmail.com>
2020-03-09 17:23:59 +0800

    [+]config-overrides

--- config-overrides.js
+++ config-overrides.js
@@ -8,6 +8,7 @@ module.exports = override(
   }),
   addLessLoader({
     javascriptEnabled: true,
-    modifyVars: { "@primary-color": "#1DA57A" }
+    modifyVars: { "@primary-color": "#298ccf" }
   }),
+  
 );
